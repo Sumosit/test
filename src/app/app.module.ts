be986b1333156ef7829b40396174443da312a4e4
Yerlan <isaev.yerlan@kase.kz>
2022-04-06 12:08:56 +0600

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { QweComponent } from './qwe/qwe.component';
+import { AsdComponent } from './asd/asd.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QweComponent
+    QweComponent,
+    AsdComponent
   ],
   imports: [
     BrowserModule
