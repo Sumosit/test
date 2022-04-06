f9de59bedf287b6580fefffb09a5bfbabde618ec
Yerlan <isaev.yerlan@kase.kz>
2022-04-06 11:55:37 +0600

    qwe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QweComponent } from './qwe/qwe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QweComponent
   ],
   imports: [
     BrowserModule
